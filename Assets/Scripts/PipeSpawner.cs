e1db38b4e8bb82c484da4003a64600eb90616b93
shayleebroadfoot <shayleebroadfoot@gmail.com>
2026-03-29 15:37:29 -0700

    Updated pipe spawner to spawn with playable gaps

--- Assets/Scripts/PipeSpawner.cs
+++ Assets/Scripts/PipeSpawner.cs
@@ -6,6 +6,8 @@ public class PipeSpawner : MonoBehaviour
     [SerializeField] private float maxTime = 1.5f;
     [SerializeField] private float heightRange = 0.45f;
     [SerializeField] private GameObject pipe;
+    private float minY = -0.2f;   // above ground
+    private float maxY = 0.8f;    // below top
 
     private float timer;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -31,7 +33,12 @@ public class PipeSpawner : MonoBehaviour
     private void SpawnPipe()
     {
         // spawn pipes randomly: position of the spawner +/- random distance (height)
-        Vector3 spawnPos = transform.position + new Vector3(0, Random.Range(-heightRange, heightRange));
+        // Vector3 spawnPos = transform.position + new Vector3(0, Random.Range(-heightRange, heightRange));
+        
+        // Updated pipe spawning logic
+        float randomY = Random.Range(minY, maxY);
+        Vector3 spawnPos = new Vector3(transform.position.x, randomY, 0);
+        
         // spawn at the location
         GameObject pipeN = Instantiate(pipe, spawnPos, Quaternion.identity);
 
